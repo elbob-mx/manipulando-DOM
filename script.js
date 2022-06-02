92fc0ca765b7fdc85c747c4ed330ef02b971e26e
Roberto <rob7th@gmail.com>
2022-06-02 00:02:43 -0500

    js on DOM

--- script.js
+++ script.js
@@ -3,9 +3,27 @@ console.log(btn);
 
 const createTask = (evento) => {
     evento.preventDefault()
-    const input = document.querySelector("[data-form-input]")
-        console.log(input.value);
+    const input = document.querySelector("[data-form-input]");
+    const value = input.value;
+    const list = document.querySelector("[data-list]")
+    const task = document.createElement("li");
+    task.classList.add("card");
+    input.value = "";
+
+    const content = `<div>
+    <i class="far fa-check-square icon"></i>
+    <span class="task">${value}</span>
+  </div>
+  <i class="fas fa-trash-alt trashIcon icon"></i>`;
+
+  task.innerHTML = content;
+
+    list.appendChild(task);
+
+    console.log(content);
 }
 
-// listener
+console.log(btn);
+
+// arrow functions
 btn.addEventListener("click", createTask);
